2f8dc537f088e727c093f5d0b050df184cfb9aea
Nambalorian <mario.imperato@gpagroup.it>
2026-06-21 18:38:06 +0200

    Init

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.26.4
 require (
 	github.com/databricks/databricks-sdk-go v0.147.0
 	github.com/rs/zerolog v1.35.1
+	github.com/stretchr/testify v1.11.1
 )
 
 require (
@@ -12,6 +13,7 @@ require (
 	cloud.google.com/go/auth/oauth2adapt v0.2.2 // indirect
 	cloud.google.com/go/compute/metadata v0.9.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-logr/logr v1.4.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
@@ -24,6 +26,7 @@ require (
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.49.0 // indirect
@@ -43,4 +46,5 @@ require (
 	google.golang.org/grpc v1.79.3 // indirect
 	google.golang.org/protobuf v1.36.10 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ sql/ops_test.go
@@ -0,0 +1,44 @@
+package sql_test
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/GPA-Gruppo-Progetti-Avanzati-SRL/tpm-databricks-common/dbricksLks"
+	"github.com/GPA-Gruppo-Progetti-Avanzati-SRL/tpm-databricks-common/sql"
+	"github.com/stretchr/testify/require"
+)
+
+func TestOps(t *testing.T) {
+	lks, err := dbricksLks.Initialize([]dbricksLks.Config{
+		{
+			Name:                "default",
+			Host:                "https://adb-6686371009486131.11.azuredatabricks.net",
+			WorkspaceResourceID: "/subscriptions/9a9b2fd9-6706-42f0-9131-5a726dffcb94/resourceGroups/SALES-INFRA-COMMON-SVIL/providers/Microsoft.Databricks/workspaces/ssalescomdatab01azne",
+			AuthType:            dbricksLks.AuthTypeAzureClientSecret,
+			ServicePrincipal: &dbricksLks.ServicePrincipal{
+				TenantID:     os.Getenv("AZURE_TENANT_ID"),
+				ClientID:     os.Getenv("AZURE_CLIENT_ID"),
+				ClientSecret: os.Getenv("AZURE_CLIENT_SECRET"),
+			},
+			WarehouseID: "2cb583d2b287b9e7",
+			Resources: []dbricksLks.Resource{
+				{
+					Id:   "smart-catalog",
+					Name: "sales_dbcatalog_ssalescomdatab01azne.smartcatalog_trusted_dss",
+				},
+			},
+		},
+	})
+	require.NoError(t, err)
+
+	b, err := sql.JsonFind(context.Background(), lks[0], "SHOW TABLES IN [dbrks:smart-catalog]")
+	require.NoError(t, err)
+	t.Log(string(b))
+
+	b, err = sql.JsonFindOne(context.Background(), lks[0], "SHOW TABLES IN [dbrks:smart-catalog]", true)
+	require.NoError(t, err)
+	t.Log(string(b))
+
+}
